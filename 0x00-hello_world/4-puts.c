9dd4ae92ac7f4fe86aaee710513f236a458a5cd6
Chemben <docbenjamen@gmail.com>
2022-09-09 13:18:36 -0700

    o

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - prints the string in the puts function
  *
- * Description: using the main function
+ * description: using the main function
  * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
