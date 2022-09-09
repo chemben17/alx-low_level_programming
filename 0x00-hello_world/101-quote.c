4b230a744df0d29468040bf10d1dbb99ea02eb26
Chemben <docbenjamen@gmail.com>
2022-09-09 13:57:49 -0700

    okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 /**
  * main -Entry point
- * Return 1
+ *
+ * description: to write "and that piece of art is useful"
+ * Return: 1
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
