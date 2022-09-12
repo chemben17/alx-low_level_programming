66a6e63cd42cc307e6fa76da48485589544e1ed2
Chemben <docbenjamen@gmail.com>
2022-09-12 13:31:23 -0700

    testing of numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,15 @@ int main (void)
 	srand(time(0));
 	n = rand() - (RAND_MAX / 2);
     /* your code goes there  */
-        if(n > 0)
-		printf("%i is positive\n", n);
+        if( n > 0)
+		printf("%d is positive\n", n);
+	
+        else if( n == 0)
+	        printf("%d is zero\n", n);
+	
 
-        else if(n == 0)
-	        printf("%i is zero\n", n);
-
-        else if(n < 0)
-		printf("%i is negative\n", n);
-
+        else if( n < 0)
+		printf("%d is negative\n", n);
+	
 	return (0);
 }
