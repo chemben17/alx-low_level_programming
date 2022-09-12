65fe3f89e9eb9bce3d0a0960d6d0373a0e49e874
Chemben <docbenjamen@gmail.com>
2022-09-12 14:26:36 -0700

    testing of numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,17 +18,17 @@ int main(void)
 
 	v = n % 10;
 
-	if( v > 5) 
+	if (v > 5) 
 	{
-		printf("Last digit of %d is %d and it is greater than 5\n", n, v);
+		printf("Last digit of %d is %d and is greater than 5\n", n, v);
 	}
-	else if( v == 0)
+	else if (v == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, v);
 	}
-	else if( v < 6 && v != 0)
+	else if (v < 6 && v != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, v);
 	}
-	return 0;
+	return (0);
 }
