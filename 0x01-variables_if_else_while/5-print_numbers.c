b76a7a32bdd21f55f9a49d5db53750e57ed54aed
Chemben <docbenjamen@gmail.com>
2022-09-12 16:22:32 -0700

    okay

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function issue
+ *
+ * description: i dont know
+ * the program prints "just what you think"
+ * Return: 0
+ */
+int main(void)
+{
+	int dig;
+	dig = 0;
+	while (dig <= 9)
+	{
+		printf("%i", dig);
+		dig++;
+	}
+	printf("\n");
+	return (0);
+}
