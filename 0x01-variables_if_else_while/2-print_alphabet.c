11e7eca57c896527df74ca54efb5aa1dea005504
Chemben <docbenjamen@gmail.com>
2022-09-12 15:18:36 -0700

    okay

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main p- main what you think
+ *
+ * description: just describe it the way yo
+ * the code prints "coding the way ok"
+ * Return: 0
+ */
+int main(void)
+{
+	int you;
+	you = 97;
+	while (you <= 122)
+	{
+		putchar(you);
+		you++;
+	}
+	putchar(10);
+	return (0);
+}
