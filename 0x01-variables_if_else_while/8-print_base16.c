9f37079fddb806a49d764efacfb6cb80a581f131
Chemben <docbenjamen@gmail.com>
2022-09-12 17:32:57 -0700

    o

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main is goof
+ *
+ * description: like i care
+ * the program prints "elo ku"
+ * Return: 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
