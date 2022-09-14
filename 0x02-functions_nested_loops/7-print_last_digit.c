ba3614ba46b7b7c5469406af3740fd2164b60e46
Chemben <docbenjamen@gmail.com>
2022-09-13 20:17:12 -0700

    o

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of the parameter passed into it
+ *
+ * @c: the argument of the function
+ * Return: idk 0
+ */
+int print_last_digit(int c)
+{
+	int remainder;
+
+	remainder = c % 10;
+
+	return (remainder);
+}
