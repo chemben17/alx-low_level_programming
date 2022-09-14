5715851129988cae4184eb641532383976bf28ab
Chemben <docbenjamen@gmail.com>
2022-09-13 19:24:57 -0700

    o

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - it prints lowecase alphabet ten times.
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int times alphabet;
+
+	for (times = 1; times <=10; times++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar(10);
+	}
+}
