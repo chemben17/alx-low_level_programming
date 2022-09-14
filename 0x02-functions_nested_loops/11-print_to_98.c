89c20e13ad49ff7223787133c9d7bc7f768a9770
Chemben <docbenjamen@gmail.com>
2022-09-13 21:35:10 -0700

    a function to add two numbers{

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints the natural no
+ * @n: the argument of the function given
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int v;
+
+	v = n;
+
+	if (n > 98)
+	{
+		for (v = n; v >= 98; v--)
+		{
+			printf("%d", v);
+			printf(" ,");
+		}
+	}
+	else
+	{
+		for (v = n; v <= 98; v++)
+		{
+			printf("%d", v);
+			printf(" ,");
+		}
+	}
+}
