cb766781ba1d004e0dbd8ccf6b8bab93ded10521
Chemben <docbenjamen@gmail.com>
2022-09-13 19:29:23 -0700

    o

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 	int times, alphabet;
 
-	for (times = 1; times <=10; times++)
+	for (times = 1; times <= 10; times++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
