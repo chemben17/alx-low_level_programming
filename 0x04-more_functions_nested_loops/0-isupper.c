d4b8576acfbeb6d3d214f844d7daa24829d320ab
Chemben <docbenjamen@gmail.com>
2022-09-15 18:46:31 -0700

    checks for lowecase alphabet

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _isupper - checks for uppercase character.
+ * int _isupper - checks for uppercase character
+ * description: Write a function that checks for u
  * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if (c == 65 && c <= 90)
+	if ((c = 65) && (c <= 90))
 	{
 		return (1);
 	}
