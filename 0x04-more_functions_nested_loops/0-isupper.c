ad374fdf980aa0bf363f3561acc023a5ac47b97f
Chemben <docbenjamen@gmail.com>
2022-09-15 19:06:10 -0700

    p

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,11 @@
 /**
  * int _isupper - checks for uppercase character
  * @c: the argument passed into the function
- * description: Write a function that checks for u
  * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if (c == 65 && c <= 90)
+	if ((c == 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
