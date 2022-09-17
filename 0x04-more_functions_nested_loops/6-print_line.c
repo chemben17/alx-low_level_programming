df940967ceafd48528ac64f88cbbaec06d0bebd6
Chemben <docbenjamen@gmail.com>
2022-09-16 20:34:36 -0700

    o

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Write a function that draws a straight line in the terminal
+ * @c: the argument
+ * Return: no return value
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 1; a <= n; a++)
+	{
+		_putchar('_');
+	}
+	if ( a <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
