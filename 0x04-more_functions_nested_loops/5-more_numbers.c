ad7b09870be7a1f99bcd3b0c36db7f9cb6b8854e
Chemben <docbenjamen@gmail.com>
2022-09-16 19:54:58 -0700

    o

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - Write a function that prints 10 times the number
+ * Return: no return value
+ */
+void more_numbers(void)
+{
+	int n, a;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar(a);
+		}
+		_putchar(10);
+	}
+	_putchar(10);
+}
