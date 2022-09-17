9fac2d6537204caea614732cf2eebbf3384557ac
Chemben <docbenjamen@gmail.com>
2022-09-16 19:26:38 -0700

    o

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  * print_numbers - Write a function that prints the numbers
+ * Return: it returns nothing
  */
 void print_numbers(void)
 {
 	int number;
+	
 	for (number = 48; number < 57; number++)
 	{
 		_putchar(number);
