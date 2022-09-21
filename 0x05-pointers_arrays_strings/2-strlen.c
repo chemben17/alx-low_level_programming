282f9ee10b9799d8dd7065abf04a0b611fe83d7b
Chemben <docbenjamen@gmail.com>
2022-09-20 20:22:50 -0700

    o

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Write a function that returns the
+ * @s: an argument
+ * Return: nothng
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	s = "Ololade!";
+	c = _strlen(s);
+
+	return (c);
+}
+
