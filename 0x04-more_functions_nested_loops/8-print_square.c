75d9ba5f254b43f2838111d13ca07b30e5ee4bb3
Chemben <docbenjamen@gmail.com>
2022-09-22 22:17:36 -0700

    drawing diagonal line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,12 +13,14 @@ void print_square(int size)
 		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < size; b++)
+			{
 				_putchar('#');
-
-			if (a == size - 1)
-				continue;
+			}
 			_putchar('\n');
 		}
 	}
-	_putchar(10);
+	else
+	{
+		_putchar('\n');
+	}
 }
