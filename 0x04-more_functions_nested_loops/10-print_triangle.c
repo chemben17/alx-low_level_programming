cf00aed9568482d398ed1a64fff9aaf5ab4204a6
Chemben <docbenjamen@gmail.com>
2022-09-24 05:28:26 -0700

    a rght angled triangle from #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_triangle - Write a function that prints a triangle
+ * @size: the triangle's height
+ * Return: none;
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	a = 1;
+
+	if (size > 0)
+	{
+		while (a <= size)
+		{
+			b = 0;
+			while (b < (n - a))
+			{
+				_putchar(' ');
+				b++;
+			}
+			b = 0;
+			while (b < a)
+			{
+				_putchar('#');
+				b++;
+			}
+			_putchar('\n');
+			a++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
