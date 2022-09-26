2bb676077af038ba2f24d95142490f1e27d20590
Chemben <docbenjamen@gmail.com>
2022-09-25 18:10:15 -0700

    changes as regards  numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ int main(void)
 		j = 0;
 		while (j < 10)
 		{
-			if (i != j && i < j)
+			if (i < j)
 			{
 				putchar(i + '0');
 				putchar(i + '0');
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that prints all possible
+ * description:  different combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if ((a + b + c) != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
