5a667314d9f87ba360845d34c97b27b5e2023a28
Chemben <docbenjamen@gmail.com>
2022-09-26 20:33:34 -0700

    o

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  */
 void puts2(char *str)
 {
-	int c = 0;
+	int i = 0;
 
-	while (*(str + c) != '\0')
+	while (str[i] != 0)
 	{
-		if (c % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(*(str + c));
+			_putchar(str[i]);
 		}
-		c++;
+		i++;
 	}
 	_putchar(10);
 }
