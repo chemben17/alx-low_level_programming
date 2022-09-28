29ea9a2ef5aaf84d01436504a77f98fba072eb9c
Chemben <docbenjamen@gmail.com>
2022-09-27 22:12:03 -0700

    o

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,3 +10,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+		dest[j] = '\0';
+	return (dest);
+}
