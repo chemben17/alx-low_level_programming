7114155a2112ff095bb6ace93d0b1d7ad1e34ff2
Chemben <docbenjamen@gmail.com>
2022-09-28 11:04:18 -0700

    the exponential product

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - a function that returns the value
+ * @x: the base number
+ * @y: the exponent number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, (y - 1)));
+}
