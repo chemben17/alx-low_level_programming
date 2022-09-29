ecec5bfdb14a88d82c24ab39882aef0acc2bdcf9
Chemben <docbenjamen@gmail.com>
2022-09-28 19:54:54 -0700

    to determine the sqr root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion -  natural square root of a numbe
+ * sqr - nothing
+ * @n: the actual number to determine
+ * @i: the varying num.
+ * Return: root or -1
+ */
+int sqr(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if ((i * i != n) && (i >= n / 2) || (n < 0))
+		return (-1);
+
+	return (sqr(n, i + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, 1));
+}
