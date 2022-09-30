6edf096bc7949de8cae3b845237fc5b3c65ee891
Chemben <docbenjamen@gmail.com>
2022-09-29 21:38:11 -0700

    printing half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ void puts_half(char *str)
 	}
 	if (i > 0)
 	{
-		if (i % 2 == '\0')
+		if (i % 2 == 0)
 		{
 			int a = (i / 2);
 			int b;
 
-			for (b = a; b <= i; b++)
+			for (b = a; b < i; b++)
 			{
 				_putchar(str[b]);
 			}
@@ -29,7 +29,7 @@ void puts_half(char *str)
 			int a = ((i - 1) / 2) + 1;
 			int b;
 
-			for (b = a; b <= i; b++)
+			for (b = a; b < i; b++)
 			{
 				_putchar(str[b]);
 			}
