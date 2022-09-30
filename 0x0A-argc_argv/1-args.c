fac508a899143210383cf6fc48ec73440569581e
Chemben <docbenjamen@gmail.com>
2022-09-30 12:59:59 -0700

    printing the exe filename

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: the number of arg passed by CLI
+ * @argv: the name passed into it CLI
+ * Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
