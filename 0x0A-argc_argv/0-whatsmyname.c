62425777b9dd49e2a23d193df4295a35bf4920d6
Chemben <docbenjamen@gmail.com>
2022-09-30 12:35:16 -0700

    printing of filename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - a program that prints its nam
+ * @argc: the number of elements of the array
+ * @argv: the array
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+}
