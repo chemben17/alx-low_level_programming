cc69e663a5c15997307b402a96e2bbd3100be617
Chemben <docbenjamen@gmail.com>
2022-09-30 13:32:00 -0700

    printing the input on the CLI

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Write a program that prints all arguments it receives
+ * @argc: the number of input arg
+ * @argv: the array of the input into CLI
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 1)
+		return (0);
+	printf("%s\n", *argv);
+	main(argc - 1, (argv + 1));
+}
