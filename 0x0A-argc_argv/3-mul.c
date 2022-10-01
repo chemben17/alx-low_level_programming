987a6ecfea92fb0805a732bf7907ae2004e64c11
Chemben <docbenjamen@gmail.com>
2022-09-30 17:30:40 -0700

    argv files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ * @argc: the count
+ * @argv: te array
+ * Return: 1 0r 0
+ */
+int main(int argc, char const *argv[])
+{
+	if (argc != 1)
+	{
+		printf("error\n");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+}
