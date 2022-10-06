b7fe4e0bcf582973d6461c4faf800c935df2dcca
Chemben <docbenjamen@gmail.com>
2022-10-05 17:34:16 -0700

    multi-dimensional array with mem alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if ((width <= 0) || (height <= 0))
 		return ('\0');
 
-	c = malloc(sizeof(**c) * (width + height));
+	c = malloc(sizeof(**c) * (width * height));
 	if (c == NULL)
 	{
 		free(c);
