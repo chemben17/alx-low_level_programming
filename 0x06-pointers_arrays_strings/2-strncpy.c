4d0e4e0ac4171479125f8730eb50cc59e40b4986
Chemben <docbenjamen@gmail.com>
2022-10-06 21:18:54 -0700

    adjusted the null chracter positioning

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
 
-	while (src[j] != '\0' && j < n)
+	while (src[j] != '\0' || j < n)
 	{
 		dest[j] = src[j];
 		j++;
 	}
-	dest[j] = '\0';
 	return (dest);
 }
