5228e904efeeb2fc6cf12d616a0cb11540931fbd
Chemben <docbenjamen@gmail.com>
2022-10-07 19:01:52 -0700

    encoding through rotting by 13 char

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - encoding
+ * @str: the stringg passed into the function
+ * Return: it returns the codefied word.
+ */
+char *rot13(char *str)
+{
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int i = 0, k;
+
+	while (str[i] != '\0')
+	{
+		for (k = 0; c[k]; k++)
+		{
+			if (str[i] == c[k])
+			{
+				str[i] = b[k];
+				break;
+			}
+			else
+				continue;
+		}
+		i++;
+	}
+	return (str);
+}
