8f239c3dc4746c50dd9b27dd73543bdb1827043c
Chemben <docbenjamen@gmail.com>
2022-10-11 21:51:59 -0700

    o

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,25 +16,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_d = malloc(sizeof(dog_t));
 	if (new_d == NULL)
 		return (NULL);
-	int namelen, ownerlen;
-
-	namelen = strlen(name);
-	ownerlen = strlen(owner);
-	new_d->name = malloc(sizeof(namelen + 1));
-	if (new_d->name == NULL)
-	{
-		free(new_d);
-		return (NULL);
-	}
-	new_d->owner = malloc(sizeof(ownerlen + 1));
-	if (new_d->owner == NULL)
-	{
-		free(new_d->name);
-		free(new_d);
-		return (NULL);
-	}
-	new_d->name = strcpy(new_d->name, name);
+	new_d->name = name;
 	new_d->age = age;
-	new_d->owner = strcpy(new_d->owner, owner);
+	new_d->owner = owner;
 	return (new_d);
 }
