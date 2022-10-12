235d58570874388c487b853e3afa1569ff337c3f
Chemben <docbenjamen@gmail.com>
2022-10-11 20:02:31 -0700

    creating an aheader file of a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
-#ifndef DOG_H_
-#define DOG_H_
-#define dog \
-	struct dog \
-{ \
-		char *name; \
-		float age; \
-		char *owner; \
-	}
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - te structure tag name
+ * @name: the member name
+ * @age: the member age
+ * @owner: the owner
+ */
+
+struct dog
+{
+		char *name;
+		float age;
+		char *owner;
+	};
+
 #endif
